aa43936531788f43fea97ccf47f31f425a5b5c85
TadeQ <tadeuszkiszka1@gmail.com>
2022-11-27 10:51:39 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,85 +1,85 @@
 ﻿//Wariant 1
 
-// using System;
+using System;
 
-// namespace FlowChart
-// {
-//     class Program
-//     {
-//         static void Main()
-//         {
-//             var Data = Console.ReadLine().Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+namespace FlowChart
+{
+    class Program
+    {
+        static void Main()
+        {
+            var Data = Console.ReadLine().Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
-//             int x = int.Parse(Data[0]);
-//             int y = int.Parse(Data[1]);
-//             int z = int.Parse(Data[2]);
+            int x = int.Parse(Data[0]);
+            int y = int.Parse(Data[1]);
+            int z = int.Parse(Data[2]);
 
-//           warunekx:
-//             if( x > 0){
-//                 if(y > 0){
-//                     x--;
-//                     y--;
-//                     Console.Write("C");
-//                     goto warunekx;
-//                 }
-//                 else{
-//                     Console.Write("D");
-//                     if(z > 0){                      
-//                         Console.Write("");
-//                     }   
-//                     else{                       
-//                         Console.Write("G");
-//                         Console.Write("");
-//                     }
-//                 }
-//             }
-//             else{
-//                 Console.Write("E");
-//                 Console.Write("G");
-//             }
-//         }
-//     }
-// }
+          warunekx:
+            if( x > 0){
+                if(y > 0){
+                    x--;
+                    y--;
+                    Console.Write("C");
+                    goto warunekx;
+                }
+                else{
+                    Console.Write("D");
+                    if(z > 0){                      
+                        Console.Write("");
+                    }   
+                    else{                       
+                        Console.Write("G");
+                        Console.Write("");
+                    }
+                }
+            }
+            else{
+                Console.Write("E");
+                Console.Write("G");
+            }
+        }
+    }
+}
 
 //Wariant 2
-// using System;
-//             namespace FlowChart
-//             {
-//                 class Program
-//                 {
-//                     static void Main()
-//                     {
-//                         var Data = Console.ReadLine().Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries);
-//                         int x = int.Parse(Data[0]);
-//                         int y = int.Parse(Data[1]);
-//                         int z = int.Parse(Data[2]);
-//                         while(x>0) {
-//                                 if(y>0){
-//                                     x--;
-//                                     y--;
-//                                     Console.Write("C");
-//                                 }
-//                                 else{
-//                                         x=0;
-//                                         if(y<=0) {
-//                                             Console.Write("D");
-//                                         if(z>0){                      
-//                                             Console.Write("");
-//                                         }   
-//                                         else{                 
-//                                                 Console.Write("G");
-//                                                 Console.Write("");
-//                                         }
-//                                         return;
-//                                         }
-//                                         return;
-//                                     }
-//                         }
-//                             Console.Write("E");
-//                             Console.Write("G");
-//                     }
-//                 }
-//             }
+using System;
+            namespace FlowChart
+            {
+                class Program
+                {
+                    static void Main()
+                    {
+                        var Data = Console.ReadLine().Trim().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                        int x = int.Parse(Data[0]);
+                        int y = int.Parse(Data[1]);
+                        int z = int.Parse(Data[2]);
+                        while(x>0) {
+                                if(y>0){
+                                    x--;
+                                    y--;
+                                    Console.Write("C");
+                                }
+                                else{
+                                        x=0;
+                                        if(y<=0) {
+                                            Console.Write("D");
+                                        if(z>0){                      
+                                            Console.Write("");
+                                        }   
+                                        else{                 
+                                                Console.Write("G");
+                                                Console.Write("");
+                                        }
+                                        return;
+                                        }
+                                        return;
+                                    }
+                        }
+                            Console.Write("E");
+                            Console.Write("G");
+                    }
+                }
+            }
 
 
 //Wariant 3
